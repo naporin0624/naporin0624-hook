442d834429f3c9f1efe42c6c8fffc2219983f9c1
naporin0624 <naporin0624@gmail.com>
2022-07-19 12:22:35 +0900

    :+1: open を props に埋め込む

--- src/react/useDialog/index.ts
+++ src/react/useDialog/index.ts
@@ -4,27 +4,27 @@ import { success, fail } from "../../result";
 
 import type { Result } from "../../result";
 
-type DialogState<T> =
-  | {
-      open: true;
-      props: T;
-    }
-  | {
-      open: false;
-      props: { [K in keyof T]?: T[K] };
-    };
+type DialogState<T> = T extends Record<string, unknown>
+  ? ({ open: true } & T) | ({ open: false } & Partial<T>)
+  : { open: true } | { open: false };
+
+const openProps = <T>(state: T): DialogState<T> => {
+  return { open: true, ...(state ? state : {}) } as DialogState<T>;
+};
 
 type Awaiter<E, T> = {
   resolve: (value: T) => void;
   reject: (error: E) => void;
 };
 
-type State<Input, Output> = DialogState<Input> &
-  Partial<Awaiter<Error, Result<Output, undefined>>>;
+type State<Input, Output> = {
+  props: DialogState<Input>;
+} & Partial<Awaiter<Error, Result<Output, undefined>>>;
 
 type RequireOrOptional<T> = T extends undefined ? [] : [value: T];
 
-type Dialog<Input, Output> = DialogState<Input> & {
+type Dialog<Input, Output> = {
+  props: DialogState<Input>;
   showDialog: (...input: RequireOrOptional<Input>) => {
     result: Promise<Result<Output, unknown>>;
     hideDialog: () => void;
@@ -41,10 +41,15 @@ export const useDialog = <
 ): Dialog<ModalInput, ModalOutput> => {
   const [dialogState, setDialogState] = useState<
     State<ModalInput, ModalOutput>
-  >(initialState ?? { open: false, props: {} });
+  >(
+    () =>
+      ({
+        props: initialState ? initialState : { open: false },
+      } as State<ModalInput, ModalOutput>)
+  );
 
   const hideDialog = useCallback(() => {
-    setDialogState(({ props }) => ({ open: false, props }));
+    setDialogState(({ props }) => ({ props: { ...props, open: false } }));
   }, []);
 
   const showDialog: Dialog<ModalInput, ModalOutput>["showDialog"] = useCallback(
@@ -55,8 +60,7 @@ export const useDialog = <
         (resolve, reject) => {
           setDialogState((previous) => ({
             ...previous,
-            open: true,
-            props: props as ModalInput,
+            props: openProps<ModalInput>(props as ModalInput),
             resolve,
             reject,
           }));
@@ -87,9 +91,7 @@ export const useDialog = <
       showDialog,
       onOk,
       onCancel,
-      ...(dialogState.open
-        ? { open: true, props: dialogState.props }
-        : { open: false, props: dialogState.props }),
+      props: dialogState.props,
     }),
     [showDialog, onOk, onCancel, dialogState]
   );
--- src/react/useDialog/useDialog.test.ts
+++ src/react/useDialog/useDialog.test.ts
@@ -7,10 +7,10 @@ import { useDialog } from ".";
 import type { Result } from "../../result";
 
 test.concurrent("set initialData", () => {
-  const props = { hoge: "huga" };
-  const { result } = renderHook(() => useDialog({ open: true, props }));
+  const props = { open: true, hoge: "huga" };
+  const { result } = renderHook(() => useDialog(props));
 
-  if (result.current.open) {
+  if (result.current.props.open) {
     expect(result.current.props).toEqual(props);
   } else {
     expect.assertions(1);
@@ -19,47 +19,48 @@ test.concurrent("set initialData", () => {
 
 test.concurrent("open dialog", () => {
   const { result } = renderHook(() => useDialog());
-  expect(result.current.open).toEqual(false);
+  expect(result.current.props.open).toEqual(false);
   act(() => {
     result.current.showDialog();
   });
 
-  expect(result.current.open).toEqual(true);
+  expect(result.current.props.open).toEqual(true);
 });
 
 test.concurrent("toggle open state", () => {
   const { result } = renderHook(() => useDialog());
-  expect(result.current.open).toEqual(false);
+  expect(result.current.props.open).toEqual(false);
 
   let hideDialog: (() => void) | undefined = undefined;
   act(() => {
     hideDialog = result.current.showDialog().hideDialog;
   });
 
-  expect(result.current.open).toEqual(true);
+  expect(result.current.props.open).toEqual(true);
 
   act(() => {
     result.current.onOk();
     hideDialog?.();
   });
-  expect(result.current.open).toEqual(false);
+
+  expect(result.current.props.open).toEqual(false);
 });
 
 test.concurrent("change props", () => {
   const { result } = renderHook(() => useDialog<{ hoge: string }>());
-  expect(result.current.props).toEqual({});
+  expect(result.current.props).toEqual({ open: false });
 
   act(() => {
     result.current.showDialog({ hoge: "hoge" });
   });
 
-  expect(result.current.props).toEqual({ hoge: "hoge" });
+  expect(result.current.props).toEqual({ hoge: "hoge", open: true });
 
   act(() => {
     result.current.showDialog({ hoge: "huga" });
   });
 
-  expect(result.current.props).toEqual({ hoge: "huga" });
+  expect(result.current.props).toEqual({ hoge: "huga", open: true });
 });
 
 test.concurrent("receive data from dialog", async () => {
